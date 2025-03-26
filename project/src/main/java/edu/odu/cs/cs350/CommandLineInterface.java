8342026dd910526ca59853f7bab6ccbe626c11d1
Daksh Patel <dpate024@odu.edu>
2025-03-26 13:24:15 -0400

    phase3

---
+++ project/src/main/java/edu/odu/cs/cs350/CommandLineInterface.java
@@ -0,0 +1,9 @@
+package edu.odu.cs.cs350;
+
+public class CommandLineInterface {
+
+    public static void main(String[] args) {
+        System.out.println("DupDetector is running!");
+        // Later: parse args, invoke token scanning, etc.
+    }
+}
